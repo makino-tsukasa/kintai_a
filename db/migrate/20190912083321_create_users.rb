2891646cf50cbd219e8bed1bdf44e4647452dee9
makino-tsukasa <aoitori.toyohashi@gmail.com>
2019-09-12 08:58:56 +0000

    Finish modeling-user

---
+++ db/migrate/20190912083321_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :affiliation
+      t.integer :employee_number
+      t.string :uid
+
+      t.timestamps
+    end
+  end
+end
