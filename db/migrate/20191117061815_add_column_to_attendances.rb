8b0280d3d3eaca240157e7ba613ec181eead1205
makino-tsukasa <aoitori.toyohashi@gmail.com>
2019-12-24 03:09:28 +0000

    2019-12-24-12:09

--- db/migrate/20191117061815_add_column_to_attendances.rb
+++ db/migrate/20191117061815_add_column_to_attendances.rb
@@ -1,6 +1,5 @@
 class AddColumnToAttendances < ActiveRecord::Migration[5.1]
   def change
     add_column :attendances, :request_to, :integer
-    add_column :attendances, :status, :enum
   end
 end
--- db/migrate/20191117120632_remove_column_from_attendances.rb
+++
@@ -1,5 +0,0 @@
-class RemoveColumnFromAttendances < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :attendances, :status, :enum
-  end
-end
